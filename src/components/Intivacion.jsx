a40d0effceec1ebde49530e5bdb3b99e51345188
Esmeralda Torres Aguirre <esmeraldatorresaguirre@MacBook-Air-de-Esmeralda.local>
2024-09-16 11:57:52 -0600

    aprueba modal boostrap

--- src/components/Intivacion.jsx
+++ src/components/Intivacion.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 
 // Components
 import FirstPage from "./invitacion/FirstPage";
@@ -544,7 +546,57 @@ const Intivacion = () => {
           </div>
           {openModal && (
             <>
-              <div className="modal-pases" tabindex="-1">
+              <Modal
+                show={openModal}
+                onHide={() => setOpenModal(false)}
+                dialogClassName="modal-90w"
+                aria-labelledby="example-custom-modal-styling-title"
+              >
+                <Modal.Header closeButton>
+                  <Modal.Title id="example-custom-modal-styling-title">
+                    Custom Modal Styling
+                  </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                  <p>
+                    Ipsum molestiae natus adipisci modi eligendi? Debitis amet
+                    quae unde commodi aspernatur enim, consectetur. Cumque
+                    deleniti temporibus ipsam atque a dolores quisquam quisquam
+                    adipisci possimus laboriosam. Quibusdam facilis doloribus
+                    debitis! Sit quasi quod accusamus eos quod. Ab quos
+                    consequuntur eaque quo rem! Mollitia reiciendis porro quo
+                    magni incidunt dolore amet atque facilis ipsum deleniti rem!
+                  </p>
+                  <p>
+                    Ipsum molestiae natus adipisci modi eligendi? Debitis amet
+                    quae unde commodi aspernatur enim, consectetur. Cumque
+                    deleniti temporibus ipsam atque a dolores quisquam quisquam
+                    adipisci possimus laboriosam. Quibusdam facilis doloribus
+                    debitis! Sit quasi quod accusamus eos quod. Ab quos
+                    consequuntur eaque quo rem! Mollitia reiciendis porro quo
+                    magni incidunt dolore amet atque facilis ipsum deleniti rem!
+                  </p>
+                  <p>
+                    Ipsum molestiae natus adipisci modi eligendi? Debitis amet
+                    quae unde commodi aspernatur enim, consectetur. Cumque
+                    deleniti temporibus ipsam atque a dolores quisquam quisquam
+                    adipisci possimus laboriosam. Quibusdam facilis doloribus
+                    debitis! Sit quasi quod accusamus eos quod. Ab quos
+                    consequuntur eaque quo rem! Mollitia reiciendis porro quo
+                    magni incidunt dolore amet atque facilis ipsum deleniti rem!
+                  </p>
+                  <p>
+                    Ipsum molestiae natus adipisci modi eligendi? Debitis amet
+                    quae unde commodi aspernatur enim, consectetur. Cumque
+                    deleniti temporibus ipsam atque a dolores quisquam quisquam
+                    adipisci possimus laboriosam. Quibusdam facilis doloribus
+                    debitis! Sit quasi quod accusamus eos quod. Ab quos
+                    consequuntur eaque quo rem! Mollitia reiciendis porro quo
+                    magni incidunt dolore amet atque facilis ipsum deleniti rem!
+                  </p>
+                </Modal.Body>
+              </Modal>
+              {/* <div className="modal-pases" tabindex="-1">
                 <div className="modal-dialog">
                   <div className="modal-content">
                     <div className="modal-header"></div>
@@ -838,6 +890,7 @@ const Intivacion = () => {
                   </div>
                 </div>
               </div>
+            */}
             </>
           )}
         </section>
